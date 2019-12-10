53e51865fc5c76bb0ea06c68d660c0f0f03aa644
mariampervez <mariam.pervez@nav.no>
2019-12-10 12:28:59 +0100

    Lagt inn behandling av oppgavemelding

--- src/main/kotlin/no/nav/eessi/pensjon/services/oppgave/OppgaveService.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/services/oppgave/OppgaveService.kt
@@ -32,17 +32,16 @@ class OppgaveService(
 
             logger.debug("mottatt oppgave melding: $melding")
 
-//            opprettOppgave(
-//                    SedType.valueOf(melding.sedType),
-//                    melding.journalpostId,
-//                    melding.tildeltEnhetsnr,
-//                    melding.aktoerId,
-//                    melding.oppgaveType,
-//                    melding.rinaSakId,
-//                    melding.filnavn,
-//                    HendelseType.valueOf(melding.hendelseType!!)
-//            )
-
+            opprettOppgave(
+                    SedType.valueOf(melding.sedType),
+                    melding.journalpostId,
+                    melding.tildeltEnhetsnr,
+                    melding.aktoerId,
+                    melding.oppgaveType,
+                    melding.rinaSakId,
+                    melding.filnavn,
+                    HendelseType.valueOf(melding.hendelseType!!)
+            )
     }
 
 
