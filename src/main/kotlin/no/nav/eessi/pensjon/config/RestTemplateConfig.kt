aa919db1f44e4131d75c8bd77fabd1807ba8d234
mariampervez <mariam.pervez@nav.no>
2022-11-30 09:42:55 +0100

    f - Legger til IOExceptionRetryInterceptor i egen "shared" pakke (skal indikere duplisering av kode)

--- src/main/kotlin/no/nav/eessi/pensjon/config/RestTemplateConfig.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/config/RestTemplateConfig.kt
@@ -4,6 +4,7 @@ import io.micrometer.core.instrument.MeterRegistry
 import no.nav.eessi.pensjon.logging.RequestIdHeaderInterceptor
 import no.nav.eessi.pensjon.logging.RequestResponseLoggerInterceptor
 import no.nav.eessi.pensjon.metrics.RequestCountInterceptor
+import no.nav.eessi.pensjon.shared.retry.IOExceptionRetryInterceptor
 import no.nav.security.token.support.client.core.ClientProperties
 import no.nav.security.token.support.client.core.oauth2.OAuth2AccessTokenService
 import no.nav.security.token.support.client.spring.ClientConfigurationProperties
@@ -15,15 +16,9 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
 import org.springframework.http.HttpRequest
 import org.springframework.http.MediaType
-import org.springframework.http.client.BufferingClientHttpRequestFactory
-import org.springframework.http.client.ClientHttpRequestExecution
-import org.springframework.http.client.ClientHttpRequestInterceptor
-import org.springframework.http.client.ClientHttpResponse
-import org.springframework.http.client.SimpleClientHttpRequestFactory
+import org.springframework.http.client.*
 import org.springframework.web.client.RestTemplate
-import java.io.IOException
 import java.util.*
-import java.util.concurrent.TimeUnit
 
 @Profile("prod", "test")
 @Configuration
@@ -72,27 +67,4 @@ class RestTemplateConfig(private val meterRegistry: MeterRegistry) {
         }
     }
 
-    internal class IOExceptionRetryInterceptor : ClientHttpRequestInterceptor {
-        private val logger = LoggerFactory.getLogger(IOExceptionRetryInterceptor::class.java)
-
-        override fun intercept(request: HttpRequest, body: ByteArray, execution: ClientHttpRequestExecution) =
-            withRetries { execution.execute(request, body) }
-
-        private fun <T> withRetries(maxAttempts: Int = 3, waitTime: Long = 1L, timeUnit: TimeUnit = TimeUnit.SECONDS, func: () -> T): T {
-            var failException: Throwable? = null
-            var count = 0
-            while (count < maxAttempts) {
-                try {
-                    return func.invoke()
-                } catch (ex: IOException) { // Dette bør ta seg av IOException - som typisk skjer der som det er nettverksissues.
-                    count++
-                    logger.warn("Attempt $count failed with ${ex.message} caused by ${ex.cause}")
-                    failException = ex
-                    Thread.sleep(timeUnit.toMillis(waitTime))
-                }
-            }
-            logger.warn("Giving up after $count attempts.")
-            throw failException!!
-        }
-    }
 }
