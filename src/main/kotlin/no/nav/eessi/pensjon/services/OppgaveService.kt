35758a440fddf2eb2d95aaa58c55cd6db93dc735
mariampervez <mariam.pervez@nav.no>
2024-03-05 15:11:46 +0100

    f - Legger til kode for å opprette oppgaver for journalpostider fra liste i gcp
    Co-authored-by: Daniel.skarpas@nav.no

--- src/main/kotlin/no/nav/eessi/pensjon/services/OppgaveService.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/services/OppgaveService.kt
@@ -1,18 +1,19 @@
 package no.nav.eessi.pensjon.services
 
 import io.micrometer.core.instrument.Metrics
-import no.nav.eessi.pensjon.eux.model.SedType
+import no.nav.eessi.pensjon.journalforing.saf.SafClient
+import no.nav.eessi.pensjon.journalforing.saf.SafRequest
 import no.nav.eessi.pensjon.metrics.MetricsHelper
-import no.nav.eessi.pensjon.models.OppgaveMelding
-import no.nav.eessi.pensjon.oppgaverouting.HendelseType
-import no.nav.eessi.pensjon.oppgaverouting.HendelseType.MOTTATT
-import no.nav.eessi.pensjon.services.Oppgave.*
-import no.nav.eessi.pensjon.services.Oppgave.OppgaveType.*
+import no.nav.eessi.pensjon.models.Journalstatus
+import no.nav.eessi.pensjon.services.gcp.GcpStorageService
 import no.nav.eessi.pensjon.utils.mapAnyToJson
+import no.nav.eessi.pensjon.utils.mapJsonToAny
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpEntity
+import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpMethod
+import org.springframework.http.MediaType
 import org.springframework.stereotype.Service
 import org.springframework.web.client.HttpStatusCodeException
 import org.springframework.web.client.RestTemplate
@@ -24,8 +25,10 @@ import java.time.format.DateTimeFormatter
  */
 @Service
 class OppgaveService(
-        private val oppgaveOAuthRestTemplate: RestTemplate,
-        @Autowired(required = false) private val metricsHelper: MetricsHelper = MetricsHelper.ForTest()
+    private val oppgaveOAuthRestTemplate: RestTemplate,
+    private val gcpStorageService: GcpStorageService,
+    private val safClient: SafClient,
+    @Autowired(required = false) private val metricsHelper: MetricsHelper = MetricsHelper.ForTest()
 ) {
     private val logger = LoggerFactory.getLogger(OppgaveService::class.java)
     private lateinit var opprettoppgave: MetricsHelper.Metric
@@ -34,21 +37,19 @@ class OppgaveService(
     }
 
     // https://oppgave.nais.preprod.local/?url=https://oppgave.nais.preprod.local/api/swagger.json#/v1oppgaver/opprettOppgave
-    fun opprettOppgaveSendOppgaveInn(oppgaveMelding: OppgaveMelding) {
+    fun opprettOppgaveSendOppgaveInn(oppgave: Oppgave) {
         opprettoppgave.measure {
-
-            val oppgave = opprettOppgave(oppgaveMelding)
 
             try {
                 val requestBody = mapAnyToJson(oppgave, true)
                 logger.info("Oppretter oppgave: $requestBody")
 
-                countEnthet(oppgaveMelding.tildeltEnhetsnr)
+                countEnthet(oppgave.tildeltEnhetsnr)
 
                 val httpEntity = HttpEntity(requestBody)
                 oppgaveOAuthRestTemplate.exchange("/", HttpMethod.POST, httpEntity, String::class.java)
 
-                logger.info("Opprettet oppgave av type ${oppgaveMelding.oppgaveType} med tildeltEnhetsnr:  ${oppgaveMelding.tildeltEnhetsnr}")
+                logger.info("Opprettet oppgave av type ${oppgave.oppgavetype} med tildeltEnhetsnr:  ${oppgave.tildeltEnhetsnr}")
             } catch(ex: HttpStatusCodeException) {
                 logger.error("En feil oppstod under opprettelse av oppgave ex: $ex body: ${ex.responseBodyAsString}")
                 throw RuntimeException("En feil oppstod under opprettelse av oppgave ex: ${ex.message} body: ${ex.responseBodyAsString}", ex)
@@ -59,58 +60,64 @@ class OppgaveService(
         }
     }
 
-    private fun opprettOppgave(opprettOppgave: OppgaveMelding): Oppgave {
-        return try {
-
-            val oppgaveTypeMap = mapOf(
-                "GENERELL" to GENERELL,
-                "JOURNALFORING" to JOURNALFORING,
-                "BEHANDLE_SED" to BEHANDLE_SED,
-                "KRAV" to KRAV,
-                "PDL" to PDL
-            )
-
-            val beskrivelse = when (oppgaveTypeMap[opprettOppgave.oppgaveType]) {
-                JOURNALFORING -> opprettGenerellBeskrivelse(opprettOppgave)
-                KRAV -> opprettGenerellBeskrivelse(opprettOppgave)
-                GENERELL -> opprettGenerellBeskrivelse(opprettOppgave)
-                BEHANDLE_SED -> behandleSedBeskrivelse(opprettOppgave)
-                PDL -> behandleSedPdlUidBeskrivelse(opprettOppgave)
-                else -> throw RuntimeException("Ukjent eller manglende oppgavetype under opprettelse av oppgave")
-            }
-
-            val oppgave = opprettGeneriskOppgave(oppgaveTypeMap, opprettOppgave, beskrivelse)
-            oppgave
-
+    private fun hentOppgave(journalpostId: String): Oppgave {
+        //TODO kalle oppgave for å hente inn oppgave vhja journalpostId
+        try {
+            return oppgaveOAuthRestTemplate.getForEntity("/api/v1/oppgaver?statuskategori=AVSLUTTET&journalpostId=$journalpostId", String::class.java)
+                .also { logger.info("Hentet oppgave for journalpostId: $journalpostId") }.body?.let { mapJsonToAny(it) }
+                    ?: throw RuntimeException("Feil ved henting av oppgave for journalpostId: $journalpostId")
         } catch (ex: Exception) {
-            logger.error("En feil oppstod under opprettelse av oppgave", ex)
+            logger.error("En feil oppstod under henting av oppgave", ex)
             throw RuntimeException(ex)
         }
     }
 
-    private fun opprettGenerellBeskrivelse(opprettOppgave: OppgaveMelding): String {
-        return opprettOppgave.sedType?.let { sedType ->
-            genererBeskrivelseTekst(
-                sedType,
-                opprettOppgave.rinaSakId,
-                opprettOppgave.hendelseType
-            )
-        } ?: throw RuntimeException("feiler med sedtype")
+    /**
+     * Skal opprette oppgaver på alle journalposter som er ferdigstilt og har en oppgave som er avsluttet
+     * Hente liste over journalposter som er under arbeid, men har avsluttede oppgaver på seg, fra gcpStorage
+     * Kalle Joark for å hente journalpostene
+     * Sjekke om oppgavene på journalpostene er ferdigstilt
+     * kalle oppgave for å hente inn oppgaven ved hjelp av journalpostIden
+     * Opprette nye oppgaver på journalpostene
+     */
+
+    fun lagOppgaveForJournalpost(): Boolean {
+        gcpStorageService.hentJournalpostFilfraS3()
+            ?.split(",")
+            ?.forEach { journalpostId ->
+                logger.info("Sjekker journalpost: $journalpostId")
+                if (erJournalpostenFerdigstilt(journalpostId)) {
+                    val oppgaveMelding = hentOppgave(journalpostId)
+                    if (oppgaveMelding.status == "FERDIGSTILT") {
+                        logger.info("Journalposten $journalpostId har en ferdigstilt oppgave")
+                        opprettOppgaveSendOppgaveInn(
+                            Oppgave(
+                                oppgavetype = "JFR",
+                                tema = oppgaveMelding.tema,
+                                prioritet = Oppgave.Prioritet.NORM.toString(),
+                                aktoerId = oppgaveMelding.aktoerId,
+                                aktivDato = LocalDate.now().format(DateTimeFormatter.ISO_DATE),
+                                journalpostId = oppgaveMelding.journalpostId,
+                                opprettetAvEnhetsnr = "9999",
+                                tildeltEnhetsnr = oppgaveMelding.tildeltEnhetsnr,
+                                fristFerdigstillelse = LocalDate.now().plusDays(1).toString(),
+                                beskrivelse = oppgaveMelding.beskrivelse
+                            )
+                        )
+                        return true
+                    }
+                }
+            }
+        return false
     }
 
-    private fun opprettGeneriskOppgave(oppgaveTypeMap: Map<String, OppgaveType>, opprettOppgave: OppgaveMelding, beskrivelse: String): Oppgave {
-        return Oppgave(
-            oppgavetype = oppgaveTypeMap[opprettOppgave.oppgaveType].toString(),
-            tema = opprettOppgave.tema,
-            prioritet = Prioritet.NORM.toString(),
-            aktoerId = opprettOppgave.aktoerId,
-            aktivDato = LocalDate.now().format(DateTimeFormatter.ISO_DATE),
-            journalpostId = opprettOppgave.journalpostId,
-            opprettetAvEnhetsnr = "9999",
-            tildeltEnhetsnr = opprettOppgave.tildeltEnhetsnr,
-            fristFerdigstillelse = LocalDate.now().plusDays(1).toString(),
-            beskrivelse = beskrivelse
-        )
+    private fun erJournalpostenFerdigstilt(journalpostId: String): Boolean {
+        val jp = safClient.hentJournalpost(journalpostId)
+        if (jp != null && jp.journalstatus == Journalstatus.UNDER_ARBEID && jp.journalpostferdigstilt == true) {
+            logger.error("Journalposten $journalpostId finnes ikke i Joark")
+            return true
+        }
+        return false
     }
 
     fun countEnthet(tildeltEnhetsnr: String?) {
@@ -120,158 +127,5 @@ class OppgaveService(
             logger.warn("Metrics feilet på enhet: $tildeltEnhetsnr")
         }
     }
-
-    /**
-     * Genererer beskrivelse i format:
-     * Utgående PXXXX - [nav på SEDen] / Rina saksnr: xxxxxx
-     */
-    private fun genererBeskrivelseTekst(sedType: SedType, rinaSakId: String, hendelseType: HendelseType): String {
-        return if(hendelseType == MOTTATT) {
-            "Inngående $sedType - ${sedType.beskrivelse} / Rina saksnr: $rinaSakId"
-        } else {
-            "Utgående $sedType - ${sedType.beskrivelse} / Rina saksnr: $rinaSakId"
-        }
-    }
-
-    fun behandleSedBeskrivelse(oppgaveMelding: OppgaveMelding): String {
-        if (oppgaveMelding.oppgaveType != "BEHANDLE_SED") return ""
-        logger.info("Genererer beskrivelse for oppgaveType behandle SED")
-
-        val filnavn = oppgaveMelding.filnavn
-        val journalpostId = oppgaveMelding.journalpostId
-        val rinaSakId = oppgaveMelding.rinaSakId
-        val aktoerId = oppgaveMelding.aktoerId
-        val sedType = oppgaveMelding.sedType
-        val behandlePBUC01eller03 = filnavn.isNullOrEmpty() && journalpostId != null && aktoerId != null
-
-        return when {
-            behandlePBUC01eller03 -> "Det er mottatt $sedType - ${sedType?.beskrivelse}, med tilhørende RINA sakId: $rinaSakId"
-            filnavn != null && journalpostId == null -> "Mottatt vedlegg: $filnavn tilhørende RINA sakId: $rinaSakId mangler filnavn eller er i et format som ikke kan journalføres. Be avsenderland/institusjon sende SED med vedlegg på nytt, i støttet filformat ( pdf, jpeg, jpg, png eller tiff ) og filnavn angitt"
-            else -> throw RuntimeException("Ukjent eller manglende parametere under opprettelse av beskrivelse for behandle SED")
-        }
-    }
-
-    fun behandleSedPdlUidBeskrivelse(oppgaveMelding: OppgaveMelding): String =
-        "Avvik i utenlandsk ID i PDL. I RINA saksnummer ${oppgaveMelding.rinaSakId} er det mottatt en SED med utenlandsk ID som er forskjellig fra den som finnes i PDL. Avklar hvilken som er korrekt eller om det skal legges til en utenlandsk ID."
 }
 
-private class Oppgave(
-        val id: Long? = null,
-        val tildeltEnhetsnr: String? = null,
-        val endretAvEnhetsnr: String? = null,
-        val opprettetAvEnhetsnr: String? = null,
-        val journalpostId: String? = null,
-        val journalpostkilde: String? = null,
-        val behandlesAvApplikasjon: String? = null,
-        val saksreferanse: String? = null,
-        val bnr: String? = null,
-        val samhandlernr: String? = null,
-        val aktoerId: String? = null,
-        val orgnr: String? = null,
-        val tilordnetRessurs: String? = null,
-        val beskrivelse: String? = null,
-        val temagruppe: String? = null,
-        val tema: String? = null,
-        val behandlingstema: String? = null,
-        val oppgavetype: String? = null,
-        val behandlingstype: String? = null,
-        val prioritet: String? = null,
-        val versjon: String? = null,
-        val mappeId: String? = null,
-        val fristFerdigstillelse: String? = null,
-        val aktivDato: String? = null,
-        val opprettetTidspunkt: String? = null,
-        val opprettetAv: String? = null,
-        val endretAv: String? = null,
-        val ferdigstiltTidspunkt: String? = null,
-        val endretTidspunkt: String? = null,
-        val status: String? = null,
-        val metadata: Map<String, String>? = null
-) {
-
-    enum class OppgaveType : Code {
-        GENERELL {
-            override fun toString() = "GEN"
-            override fun decode() = "Generell"
-        },
-        JOURNALFORING {
-            override fun toString() = "JFR"
-            override fun decode() = "Journalføringsoppgave"
-        },
-        BEHANDLE_SED {
-            override fun toString() = "BEH_SED"
-            override fun decode() = "Behandle SED"
-        },
-        KRAV {
-            override fun toString() = "KRA"
-            override fun decode() = "Krav"
-        },
-        PDL {
-            override fun toString() = "BEH_SED"
-            override fun decode() = "Behandle SED"
-        };
-
-    }
-
-    enum class Tema : Code {
-        PENSJON {
-            override fun toString() = "PEN"
-            override fun decode() = "Pensjon"
-        },
-        UFORETRYGD {
-            override fun toString() = "UFO"
-            override fun decode() = "Uføretrygd"
-        },
-        OMSTILLING {
-            override fun toString() = "EYO"
-            override fun decode() = "Omstilling"
-        },
-        BARNEPENSJON {
-            override fun toString() = "EYB"
-            override fun decode() = "Barnepensjon"
-        }
-    }
-
-    enum class Behandlingstema : Code {
-        UTLAND {
-            override fun toString() = "ab0313"
-            override fun decode() = "Utland"
-        },
-        UFORE_UTLAND {
-            override fun toString() = "ab0039"
-            override fun decode() = "Uføreytelser fra utlandet"
-        }
-    }
-
-    enum class Temagruppe : Code {
-        PENSJON {
-            override fun toString() = "PENS"
-            override fun decode() = "Pensjon"
-        },
-        UFORETRYDG {
-            override fun toString() = "UFRT"
-            override fun decode() = "Uføretrydg"
-        }
-    }
-
-    enum class Behandlingstype : Code {
-        MOTTA_SOKNAD_UTLAND {
-            override fun toString() = "ae0110"
-            override fun decode() = "Motta søknad utland"
-        },
-        UTLAND {
-            override fun toString() = "ae0106"
-            override fun decode() = "Utland"
-        }
-    }
-
-    enum class Prioritet {
-        HOY,
-        NORM,
-        LAV
-    }
-
-    interface Code {
-        fun decode(): String
-    }
-}
