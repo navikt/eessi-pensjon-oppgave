2af745977ec73ea22671f269ef3514fa40fb6c6a
Daniel Skarpås <dskarpas@gmail.com>
2024-03-08 11:42:34 +0100

    F - Henter direkte fra blob

--- src/main/kotlin/no/nav/eessi/pensjon/services/gcp/GcpStorageService.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/services/gcp/GcpStorageService.kt
@@ -40,7 +40,7 @@ class GcpStorageService( @param:Value("\${GCP_BUCKET_NAME}") var bucketname: Str
         val jsonHendelse =  gcpStorage.get(BlobId.of(bucketname, "journalpostIderSomTrengerNyeOppgaver.txt"))
 
         if(jsonHendelse!= null && jsonHendelse.exists()) {
-            return jsonHendelse.getContent().toString()
+            return jsonHendelse.toString()
         }
         return null
     }
