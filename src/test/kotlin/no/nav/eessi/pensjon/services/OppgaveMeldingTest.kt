65a851d67f6ff66df00ed6505cc2e5614e0fa35b
Daniel Skarpås <dskarpas@gmail.com>
2024-03-15 09:04:31 +0100

    Revert "F - Endrer dato til 1 døgn"
    This reverts commit b0f529a1c89b25e1495142f347af6cdcd5d6c3c0.

--- src/test/kotlin/no/nav/eessi/pensjon/services/OppgaveMeldingTest.kt
+++ src/test/kotlin/no/nav/eessi/pensjon/services/OppgaveMeldingTest.kt
@@ -1,15 +1,8 @@
 package no.nav.eessi.pensjon.services
 
-import io.mockk.every
-import io.mockk.mockk
-import no.nav.eessi.pensjon.models.Oppgave
 import no.nav.eessi.pensjon.models.OppgaveMelding
-import no.nav.eessi.pensjon.utils.mapAnyToJson
 import no.nav.eessi.pensjon.utils.mapJsonToAny
 import org.junit.jupiter.api.Test
-import org.springframework.core.env.Environment
-import java.nio.file.Files
-import java.nio.file.Paths
 
 class OppgaveMeldingTest {
 
@@ -30,10 +23,5 @@ class OppgaveMeldingTest {
         OppgaveMelding.fromJson(json)
 
         mapJsonToAny<OppgaveMelding>(json)
-    }
-
-    @Test
-    fun readAndProcessFile(){
-        OppgaveForJournalpost(mockk(), mockk(), mockk(), mockk<Environment>().apply { every { activeProfiles } returns arrayOf("PROD") }).lagOppgaver()
     }
 }
